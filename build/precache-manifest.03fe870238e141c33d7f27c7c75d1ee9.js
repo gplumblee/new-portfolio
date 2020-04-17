031c6a26b443cec96a6636c7c59092a1e5952866
Greg Plumblee <gplumblee@hotmail.com>
2020-04-17 12:03:57 -0700

    removed yarn

---
+++ build/precache-manifest.03fe870238e141c33d7f27c7c75d1ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d893968b74ca809185399171bb37d367",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa7a5c8cd36c3ba48c46",
+    "url": "/static/css/main.288e363c.chunk.css"
+  },
+  {
+    "revision": "396cf62dc93c9e4f67fc",
+    "url": "/static/js/2.e350ed5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e350ed5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7a5c8cd36c3ba48c46",
+    "url": "/static/js/main.357781e2.chunk.js"
+  },
+  {
+    "revision": "5fcbf2371af52303c5ef",
+    "url": "/static/js/runtime-main.c226d6e8.js"
+  }
+]);
